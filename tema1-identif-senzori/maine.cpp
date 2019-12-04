2fb193802dfda4f251eaf58c6bccd026ad9da5a7
IonutF <ionut.filea@gmail.com>
2019-12-05 00:19:17 +0200

    Initial commit

---
+++ tema1-identif-senzori/maine.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	std::string linie;
+	std::ifstream sensors("data/sensors.txt");
+	//sensors.open("/data/sensors.txt");
+	if (sensors.is_open())
+	{
+		std::cout << "Lista sezorilor cu temperaturile aferente:" << '\n';
+		std::cout << "" << '\n';
+		while (getline(sensors, linie))
+		{
+			std::cout << linie << '\n';
+		}
+		sensors.close();
+	}
+	else
+	{
+		std::cout << "File is not there" << '\n';
+		std::cout << "Please re-write your code" << '\n';
+	}
+	std::cin.get();
+	return 0;
+}
